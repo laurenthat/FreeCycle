955de25811e31b04326ce259f4a275836c6cbf34
darjapo <darjapo@metropolia.fi>
2023-02-19 05:13:58 +0200

    basic home screen filtering aka search done

--- components/List.js
+++ components/List.js
@@ -1,36 +1,39 @@
-import {FlatList, StyleSheet} from 'react-native';
-import {useMedia} from '../hooks/ApiHooks';
-import ListItem from './ListItem';
-import PropTypes from 'prop-types';
-import {useContext, useEffect, useState} from 'react';
-import {MainContext} from '../contexts/MainContext';
+import { FlatList } from "react-native";
+import { useMedia } from "../hooks/ApiHooks";
+import ListItem from "./ListItem";
+import PropTypes from "prop-types";
+import { useContext, useEffect, useState } from "react";
+import { MainContext } from "../contexts/MainContext";
 
-const List = ({navigation, myFilesOnly = false}) => {
-  const {update, setUpdate} = useContext(MainContext);
-  const {mediaArray} = useMedia(myFilesOnly);
+const List = ({ navigation, myFilesOnly = false, input, setInput }) => {
+  const { update, setUpdate } = useContext(MainContext);
+  const { mediaArray } = useMedia(myFilesOnly);
   const [fetching, setFetching] = useState(false);
   const unRefresh = () => {
     setUpdate(!update);
     setFetching(true);
-  }
-  useEffect(() => {setFetching(false)},[mediaArray]);
+  };
+  useEffect(() => {
+    setFetching(false);
+  }, [mediaArray]);
   return (
     <FlatList
       refreshing={fetching}
       onRefresh={unRefresh}
       data={mediaArray}
       keyExtractor={(item, index) => index.toString()}
-      renderItem={({item}) => (
-        <ListItem navigation={navigation} singleMedia={item} />
-        )}
+      renderItem={({ item }) => {
+        if (input == "" || item.title.toLowerCase().includes(input.toLowerCase()) || item.description.toLowerCase().includes(input.toLowerCase())) {
+          return <ListItem navigation={navigation} singleMedia={item} />;
+        }
+      }}
     />
   );
 };
 
 List.propTypes = {
   navigation: PropTypes.object.isRequired,
-  myFilesOnly : PropTypes.bool,
+  myFilesOnly: PropTypes.bool,
 };
-
 
 export default List;
--- views/Home.js
+++ views/Home.js
@@ -1,26 +1,36 @@
-import {Platform, StyleSheet, SafeAreaView} from 'react-native';
-import List from '../components/List';
-import PropTypes from 'prop-types';
+import * as React from 'react';
+import { Platform, StyleSheet, SafeAreaView } from "react-native";
+import List from "../components/List";
+import PropTypes from "prop-types";
+import { Searchbar } from 'react-native-paper';
 
-const Home = ({navigation}) => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <List  navigation={navigation}/>
-        </SafeAreaView>
-    );
+const Home = ({ navigation }) => {
+  const [searchQuery, setSearchQuery] = React.useState("");
 
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+      <List input={searchQuery} setInput={setSearchQuery} navigation={navigation} />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      paddingTop: Platform.OS === 'android' ? 30 : 0,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? 30 : 0,
+  },
 });
 
 Home.propTypes = {
-    navigation: PropTypes.object,
-  };
+  navigation: PropTypes.object,
+};
 
 export default Home;
