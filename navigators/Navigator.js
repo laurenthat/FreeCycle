15d825d04b37ce11fdb6e7b80330458dd1d17b2a
sallyzy0123 <sally_zy@protonmail.com>
2023-03-06 15:38:26 +0200

    style the nav bar

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -14,124 +14,90 @@ import MyFiles from '../views/MyFiles';
 import Modify from '../views/Modify';
 import Notification from '../views/Notification';
 import Search from '../views/Search';
+import {TouchableOpacity, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+const CustomTabBarButton = ({children, onPress}) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      top: -25,
+      justifyContent: 'center',
+      ...styles.shadow,
+    }}
+  >
+    <View
+      style={{
+        width: 70,
+        height: 70,
+        borderWidth: 5,
+        borderColor: 'white',
+        borderRadius: 35,
+        backgroundColor: '#fdaa5e',
+      }}
+    >
+      {children}
+    </View>
+  </TouchableOpacity>
+);
+
 const TabScreen = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={() => ({
+        tabBarActiveTintColor: '#fdaa5e',
+        tabBarInactiveTintColor: 'gray',
+        tabBarShowLabel: false,
+      })}
+    >
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({color}) => <Icon name="home" color={color} />,
+          tabBarIcon: ({color}) => <Icon name="home" color={color} size="30" />,
         }}
       />
       <Tab.Screen
         name="Search"
         component={Search}
         options={{
-          tabBarIcon: ({color}) => <Icon name="search" color={color} />,
+          tabBarIcon: ({color}) => (
+            <Icon name="search" color={color} size="30" />
+          ),
         }}
       />
       <Tab.Screen
         name="Upload"
         component={Upload}
         options={{
-          tabBarIcon: ({color}) => <Icon name="cloud-upload" color={color} />,
+          tabBarIcon: () => <Icon name="add" size="50" color="white" />,
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
         }}
       />
       <Tab.Screen
         name="Notification"
         component={Notification}
         options={{
-          tabBarIcon: ({color}) => <Icon name="notifications" color={color} />,
+          tabBarIcon: ({color}) => (
+            <Icon name="notifications" color={color} size="30" />
+          ),
         }}
       />
       <Tab.Screen
         name="Profile"
         component={Profile}
         options={{
-          tabBarIcon: ({color}) => <Icon name="person" color={color} />,
+          tabBarIcon: ({color}) => (
+            <Icon name="person" color={color} size="30" />
+          ),
         }}
       />
     </Tab.Navigator>
   );
 };
-
-// const Tabs = createBottomTabNavigator(
-//   {
-//     Profile: {
-//       screen: Profile,
-//       navigationOptions: {
-//         title: 'Profile',
-//         tabBarLabel: 'Profile',
-//         tabBarIcon: ({tintColor}) => (
-//           <Icon
-//             name="ios-settings-outline"
-//             type="ionicon"
-//             size={33}
-//             color={tintColor}
-//           />
-//         ),
-//       },
-//     },
-//     Charities: {
-//       screen: Upload,
-//       navigationOptions: {
-//         title: 'Browse',
-//         tabBarLabel: 'Browse',
-//         tabBarIcon: ({tintColor}) => (
-//           <View
-//             style={{
-//               height: 80,
-//               width: 80,
-//               borderRadius: 100,
-//               backgroundColor: '#FE6D64',
-//               paddingTop: 15,
-//             }}
-//           >
-//             <Icon
-//               name="ios-heart-outline"
-//               type="ionicon"
-//               size={45}
-//               color={tintColor}
-//             />
-//           </View>
-//         ),
-//       },
-//     },
-//     Account: {
-//       screen: Home,
-//       navigationOptions: {
-//         title: 'Account',
-//         tabBarLabel: 'Account',
-//         tabBarIcon: ({tintColor}) => (
-//           <Icon
-//             name="connectdevelop"
-//             type="font-awesome"
-//             size={25}
-//             color={tintColor}
-//           />
-//         ),
-//       },
-//     },
-//   },
-//   {
-//     tabBarOptions: {
-//       activeTintColor: '#84E1BF',
-//       inactiveTintColor: 'white',
-//       labelStyle: {
-//         fontSize: 12,
-//       },
-//       style: {
-//         backgroundColor: '#283940',
-//       },
-//       showLabel: false,
-//     },
-//   }
-// );
 
 const StackScreen = () => {
   const {isLoggedIn} = useContext(MainContext);
@@ -161,6 +127,23 @@ const Navigator = () => {
       <StackScreen />
     </NavigationContainer>
   );
+};
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#7f5df0',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
+
+Navigator.propTypes = {
+  children: PropTypes.object,
 };
 
 export default Navigator;
